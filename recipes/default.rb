db88edfead92f3770f9b49583d1d7b26e6727ef6
Chris Dutton <chris@aquatic-edge.com>
2014-01-07 18:26:44 -0800

    Add package nodejs

--- recipes/default.rb
+++ recipes/default.rb
@@ -41,6 +41,10 @@ include_recipe "rvm::system"
 include_recipe "rvm_passenger::default"
 include_recipe "rvm_passenger::apache2"
 
+package "nodejs" do
+	action :install
+end
+
 # NOTE: Set up a role, and define all the attributes required
 # by the above recipes. For example:
 #
